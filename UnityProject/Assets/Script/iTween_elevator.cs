d0a169dcebfd9cda19815f5a035526aa78824a88
Evyle <les_sims_34@hotmail.fr>
2016-02-23 14:53:03 +0100

    ajout de la scène du bâtiment (Martin)

---
+++ UnityProject/Assets/Script/iTween_elevator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class iTween_elevator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("elevator"), "time", 10, "easetype", iTween.EaseType.linear, "loopType", "loop", "delay", 0));
+   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+     }
+}
