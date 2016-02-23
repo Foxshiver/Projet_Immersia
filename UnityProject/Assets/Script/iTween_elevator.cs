56e10c6245c3d84c55351cb946df0b816f19a930
Evyle <les_sims_34@hotmail.fr>
2016-02-23 18:07:04 +0100

    ascenseur fonctionnel

--- UnityProject/Assets/Script/iTween_elevator.cs
+++ UnityProject/Assets/Script/iTween_elevator.cs
@@ -5,9 +5,18 @@ public class iTween_elevator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("elevator"), "time", 10, "easetype", iTween.EaseType.linear, "loopType", "loop", "delay", 0));
-   
 	}
+    /*
+    void OnCollisionEnter()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("elevatorUp"), "time", 10));
+    }*/
+
+    void OnTriggerEnter()
+    {
+        if(this.GetComponent<iTween>() == null)
+            iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("elevatorUp"), "time", 10));
+    }
 	
 	// Update is called once per frame
 	void Update () {
